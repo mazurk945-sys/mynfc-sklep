7ac8c63870178b6540f5317ce077aadacfc42335
mazurk945-sys <mazurk945@gmail.com>
2026-07-19 23:13:19 +0200

    Delete app directory

--- app/layout.tsx
+++
@@ -1,8 +0,0 @@
-import "./globals.css";
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="pl">
-      <body style={{background: 'black', color: 'white', margin: 0}}>{children}</body>
-    </html>
-  );
-}
